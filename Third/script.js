ebe5654135e7a910881113dc76c55c7d81bb53d7
EternalCodeWriter <149813570+EternalCodeWriter@users.noreply.github.com>
2024-02-03 09:35:14 +0530

    Third Project

---
+++ Third/script.js
@@ -0,0 +1,26 @@
+function nightmode(){
+    $('body').css({"background-color":"black","color":"white"});
+}
+function daymode(){
+    $('body').css({"background-color":"white","color":"black"});
+}
+
+
+
+
+/* function NightMode(){
+    $('.content').css({"background-color":"black","color":"wheat"});
+    $('.content1').css({"background-color":"rgb(24, 15, 15)","color":"white"});
+    $('body').css("background-color","grey");
+    $('.btn').css({"background-color":"gray","color":"white"});
+    $('input').css("background-color","purple");
+    /* alert("NightMode"); 
+}*/
+
+
+
+
+
+
+
+
